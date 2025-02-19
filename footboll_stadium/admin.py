60044fdb61f30067ae48ef5173ae4650a94d544d
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-19 10:06:07 +0500

    update stadium admin

--- footboll_stadium/admin.py
+++ footboll_stadium/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from footboll_stadium.models import FootballStadium
 
 
-class FootballFieldAdmin(admin.ModelAdmin):
+class FootballStadiumAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'address', 'contact', 'status', 'created_at')
     search_fields = ('name', 'address')
     list_filter = ('address', 'created_at')
     ordering = ('-created_at',)
 
 
-admin.site.register(FootballStadium, FootballFieldAdmin)
+admin.site.register(FootballStadium, FootballStadiumAdmin)
