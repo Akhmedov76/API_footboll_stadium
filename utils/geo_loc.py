58980bfd112cec5dfdfbfc0d9d5e591d65a8f6bc
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-23 20:41:44 +0500

    update utils

--- utils/geo_loc.py
+++ utils/geo_loc.py
@@ -1,17 +1,25 @@
 from scipy.spatial import distance
 from math import sin, cos, sqrt, atan2, radians
 
+
 def get_distance(point1, point2):
     R = 6371
-    lat1 = radians(point1[0])  #insert value
+    lat1 = radians(point1[0])  # insert value
     lon1 = radians(point1[1])
     lat2 = radians(point2[0])
     lon2 = radians(point2[1])
 
     dlon = lon2 - lon1
-    dlat = lat2- lat1
+    dlat = lat2 - lat1
 
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c
     return distance
+
+# Example
+# point1 = (52.2296756, 21.0122287)
+# point2 = (48.856614, 2.3522219)
+#
+# distance_in_km = get_distance(point1, point2)
+# print(f"Distance  {point1} and {point2}  {distance_in_km} km")
--- utils/geo_near.py
+++ utils/geo_near.py
@@ -5,3 +5,15 @@ def calculate_distance(stadion_lan, stadion_lot, user_lan, user_lon):
     stadion_coordinates = (stadion_lan, stadion_lot)
     user_coordinates = (user_lan, user_lon)
     return geodesic(stadion_coordinates, user_coordinates).km
+
+
+# Example
+
+
+# stadion_lan = 52.2296756
+# stadion_lot = 21.0122287
+# user_lan = 48.856614
+# user_lon = 2.3522219
+#
+# distance_in_km = calculate_distance(stadion_lan, stadion_lot, user_lan, user_lon)
+# print(f"Distance from Stadion to User: {distance_in_km} km")
