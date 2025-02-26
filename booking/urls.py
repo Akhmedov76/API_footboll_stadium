748db14ba2093212bd79931815a14b4aeeea2eff
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-26 15:03:56 +0500

    update urls

--- booking/urls.py
+++ booking/urls.py
@@ -12,6 +12,6 @@ router.register(r'booking', BookingViewSet, basename='order')
 urlpatterns = [
     path('', include(router.urls)),
     path('booking/<int:booking_id>/confirm/', confirm_booking, name='confirm-booking'),
-    path('v1-booking/', BookingsView.as_view(), name='bookings'),
-    path('v1-booking/<int:booking_id>/', BookingsView.as_view()),
+    path('v1/booking/', BookingsView.as_view(), name='bookings'),
+    path('v1/booking/<int:booking_id>/', BookingsView.as_view()),
 ]
